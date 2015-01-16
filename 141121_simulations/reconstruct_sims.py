2e6a4399edc2c8c801efa1f98592321e7c0f5a6f
Arne de Laat <adelaat@nikhef.nl>
2015-01-16 15:46:22 +0100

    Run simulations and reconstruct resulting events
    Compare input to reconstructed directions

---
+++ 141121_simulations/reconstruct_sims.py
@@ -0,0 +1,47 @@
+""" Reconstruct the results
+
+"""
+
+
+import tables
+from numpy import histogram, pi, array, arange, linspace
+
+from sapphire.analysis.reconstructions import (ReconstructESDEvents,
+                                               ReconstructESDCoincidences)
+
+
+RESULT_PATH = 'result_400_gen_16394.h5'
+
+
+def reconstruct_simulations():
+    with tables.open_file(RESULT_PATH, 'a') as data:
+        cluster = data.root.coincidences._v_attrs.cluster
+
+        for station in cluster.stations:
+            station_group = '/cluster_simulations/station_%d' % station.number
+            rec_events = ReconstructESDEvents(data, station_group, station,
+                                              overwrite=True, progress=True)
+            rec_events.prepare_output()
+            rec_events.offsets = station.detector_offsets
+            rec_events.store_offsets()
+            try:
+                rec_events.reconstruct_directions()
+                rec_events.store_reconstructions()
+            except:
+                pass
+
+        rec_coins = ReconstructESDCoincidences(data, '/coincidences',
+                                               overwrite=True, progress=True)
+        rec_coins.prepare_output()
+        rec_coins.offsets = {station.number: [o + station.gps_offset
+                                              for o in station.detector_offsets]
+                             for station in cluster.stations}
+        try:
+            rec_coins.reconstruct_directions()
+            rec_coins.store_reconstructions()
+        except:
+            pass
+
+
+if __name__ == '__main__':
+    reconstruct_simulations()
