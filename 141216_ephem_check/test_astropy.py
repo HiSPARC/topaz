fb929b3964bca66fe55fe17232f955757782f1fc
Arne de Laat <adelaat@nikhef.nl>
2015-02-05 18:16:18 +0100

    Add calculations using SAPPHiRE
    Enable print functions to compare the outputs
    Almost equal, however, a large 0.55° offset in the IDL example..

--- 141216_ephem_check/test_astropy.py
+++ 141216_ephem_check/test_astropy.py
@@ -14,12 +14,19 @@ higher-precision checks on 64-bit machines.
 """
 from __future__ import (absolute_import, division, print_function,
                         unicode_literals)
+import calendar
+import datetime
+
+import numpy as np
+
 from astropy.tests.helper import pytest, catch_warnings
 from astropy import units as u
 from astropy.time import Time, TimeDelta
 from astropy.coordinates.builtin_frames import AltAz
 from astropy.coordinates import EarthLocation
 from astropy.coordinates import Angle, SkyCoord
+
+from sapphire.transformations import base, angles, celestial, clock
 
 
 def test_against_hor2eq():
@@ -28,6 +35,7 @@ def test_against_hor2eq():
     See EXAMPLE input and output here:
     http://idlastro.gsfc.nasa.gov/ftp/pro/astro/hor2eq.pro
     """
+    print('IDL hor2eq')
     # Observatory position for `kpno` from here:
     # http://idlastro.gsfc.nasa.gov/ftp/pro/astro/observatory.pro
     location = EarthLocation(lon=Angle('-111d36.0m'),
@@ -48,11 +56,12 @@ def test_against_hor2eq():
     # because the observation date is in the future
     with catch_warnings() as _:
         radec_actual = altaz.transform_to(radec_frame)
+    print('Astropy: ', radec_actual)
+
 
     radec_expected = SkyCoord('00h13m14.1s  +15d11m0.3s', frame=radec_frame)
+    print('Source:  ', radec_expected)
     distance = radec_actual.separation(radec_expected).to('arcsec')
-    # print(radec_expected)
-    # print(radec_actual)
     # print(distance)
 
     # TODO: why is there a difference of 2.6 arcsec currently?
@@ -60,6 +69,23 @@ def test_against_hor2eq():
     # radec_actual = ra=3.3094193224314625 deg, dec=15.183757021354532 deg
     # distance = 2.6285 arcsec
     assert distance < 5 * u.arcsec
+
+    # SAPPHiRE
+    longitude = -111.6
+    latitude = 31.9633
+    jd = 2466879.7083333
+    elevation = (37, 54, 41)
+    azi = (264, 55, 6)
+    # lst = clock.gmst_to_lst(clock.juliandate_to_gmst(jd), longitude)
+    # Matches  LAST = +03 53 53.6  in the hor2eq.pro
+    gps = clock.utc_to_gps(calendar.timegm(clock.juliandate_to_utc(jd).utctimetuple()))
+    zenith, azimuth = celestial.horizontal_to_zenithazimuth(
+        np.radians(base.sexagesimal_to_decimal(*elevation)),
+        np.radians(base.sexagesimal_to_decimal(*azi)))
+    ra, dec = celestial.zenithazimuth_to_equatorial(longitude, latitude,
+                                                    gps, zenith, azimuth)
+
+    print('SAPPHiRE: ra=%f, dec=%f' % (np.degrees(ra), np.degrees(dec)))
 
 
 def test_against_pyephem():
@@ -71,6 +97,7 @@ def test_against_pyephem():
     https://gist.github.com/zonca/1672906
     https://github.com/phn/pytpm/issues/2#issuecomment-3698679
     """
+    print('PyEphem')
     obstime = Time('2011-09-18 08:50:00')
     location = EarthLocation(lon=Angle('-109d24m53.1s'),
                              lat=Angle('33d41m46.0s'),
@@ -83,8 +110,10 @@ def test_against_pyephem():
 
     altaz = SkyCoord('6.8927d -60.7665d', frame=altaz_frame)
     radec_actual = altaz.transform_to('icrs')
+    print('Astropy: ', radec_actual)
 
     radec_expected = SkyCoord('196.497518d -4.569323d', frame='icrs')  # EPHEM
+    print('Source:  ', radec_expected)
     # radec_expected = SkyCoord('196.496220d -4.569390d', frame='icrs')  # HORIZON
     distance = radec_actual.separation(radec_expected).to('arcsec')
     # TODO: why is this difference so large?
@@ -97,6 +126,19 @@ def test_against_pyephem():
     # Current value: 0.0031402822944751997 arcsec
     assert distance < 1 * u.arcsec
 
+    # SAPPHiRE
+    longitude = base.sexagesimal_to_decimal(-109, 24, 53.1)
+    latitude = base.sexagesimal_to_decimal(33, 41, 46.0)
+    utc = datetime.datetime(2011, 9, 18, 8, 50)
+    elevation = np.radians(-60.7665)
+    azi = np.radians(6.8927)
+    gps = clock.utc_to_gps(calendar.timegm(utc.utctimetuple()))
+    zenith, azimuth = celestial.horizontal_to_zenithazimuth(elevation, azi)
+    ra, dec = celestial.zenithazimuth_to_equatorial(longitude, latitude,
+                                                    gps, zenith, azimuth)
+
+    print('SAPPHiRE: ra=%f, dec=%f' % (np.degrees(ra), np.degrees(dec)))
+
 
 def test_against_jpl_horizons():
     """Check that Astropy gives consistent results with the JPL Horizons example.
@@ -105,6 +147,7 @@ def test_against_jpl_horizons():
     (from the first row of the Results table at the bottom of that page)
     http://ssd.jpl.nasa.gov/?horizons_tutorial
     """
+    print('NASA JPL')
     obstime = Time('1998-07-28 03:00')
     location = EarthLocation(lon=Angle('248.405300d'),
                              lat=Angle('31.9585d'),
@@ -118,11 +161,27 @@ def test_against_jpl_horizons():
 
     altaz = SkyCoord('143.2970d 2.6223d', frame=altaz_frame)
     radec_actual = altaz.transform_to('icrs')
+    print('Astropy: ', radec_actual)
     radec_expected = SkyCoord('19h24m55.01s -40d56m28.9s', frame='icrs')
+    print('Source:  ', radec_expected)
     distance = radec_actual.separation(radec_expected).to('arcsec')
     # TODO: why is this difference so large?
     # It currently is: 557.2748864283525 arcsec
     assert distance < 1e4 * u.arcsec
+
+    # SAPPHiRE
+    longitude = 248.405300
+    latitude = 31.9585
+    utc = datetime.datetime(1998, 7, 28, 3, 0)
+    elevation = np.radians(2.6223)
+    azi = np.radians(143.2970)
+    gps = clock.utc_to_gps(calendar.timegm(utc.utctimetuple()))
+    zenith, azimuth = celestial.horizontal_to_zenithazimuth(elevation, azi)
+    ra, dec = celestial.zenithazimuth_to_equatorial(longitude, latitude,
+                                                    gps, zenith, azimuth)
+
+    print('SAPPHiRE: ra=%f, dec=%f' % (np.degrees(ra), np.degrees(dec)))
+
 
 
 if __name__ == '__main__':
