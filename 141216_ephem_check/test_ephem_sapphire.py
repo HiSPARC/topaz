a546cd0cb3873b2138b7084fb021fd7cb410679f
Arne de Laat <adelaat@nikhef.nl>
2015-02-18 09:30:35 +0100

    Add Astropy comparison

--- 141216_ephem_check/test_ephem_sapphire.py
+++ 141216_ephem_check/test_ephem_sapphire.py
@@ -3,6 +3,12 @@ from datetime import datetime
 import numpy as np
 import ephem
 
+from astropy import units as u
+from astropy.time import Time, TimeGPS
+from astropy.coordinates.builtin_frames import AltAz
+from astropy.coordinates import EarthLocation
+from astropy.coordinates import Angle, SkyCoord
+
 from sapphire.transformations import base, angles, celestial, clock
 
 
@@ -17,6 +23,8 @@ H_ALTITUDE, H_AZIMUTH = celestial.zenithazimuth_to_horizontal(ZENITH, AZIMUTH)
 
 GPS = 1333018296.870008589
 UTC = clock.gps_to_utc(GPS)
+GPS_EPOCH = 315964800.000000000
+
 
 
 def calc_ephem():
@@ -35,13 +43,29 @@ def calc_ephem():
     observer.date = ephem.Date(datetime.utcfromtimestamp(UTC))
 
     ra, dec = observer.radec_of(H_AZIMUTH, H_ALTITUDE)
-    print 'Ephem:   ', ra, dec
-    print 'Ephem:    %10.6f %10.6f' % (ra.real, dec.real)
-    print 'Ephem:    %10.6f %10.6f' % (np.degrees(ra.real), np.degrees(dec.real))
+#     print 'Ephem:   ', ra, dec
+#     print 'Ephem:    %10.6f %10.6f' % (ra.real, dec.real)
+    print 'Ephem:     %10.6f %10.6f' % (np.degrees(ra.real), np.degrees(dec.real))
 
 #     e = ephem.Equatorial(ra, dec, epoch='2000')
 #     g = ephem.Galactic(e)
 #     print 'Galactic', g.lon, g.lat
+
+def calc_astropy():
+    """Caclulate coordinates using AstroPy
+
+    Code from the test code for AzAlt in AstroPy
+
+    """
+    obstime = Time(GPS - GPS_EPOCH, format='gps')
+    location = EarthLocation(lon=Angle('%fd' % LONGITUDE),
+                             lat=Angle('%fd' % LATITUDE),
+                             height=51.4433 * u.m)
+    altaz_frame = AltAz(obstime=obstime, location=location)
+    altaz = SkyCoord('%fd %fd' % (np.degrees(H_AZIMUTH), np.degrees(H_ALTITUDE)),
+                     frame=altaz_frame)
+    radec = altaz.transform_to('icrs')
+    print 'Astropy:   %10.6f %10.6f' % (radec.frame.ra.deg, radec.frame.dec.deg)
 
 
 def calc_sapphire():
@@ -54,12 +78,14 @@ def calc_sapphire():
     sra = base.decimal_to_sexagesimal(angles.radians_to_hours(ra))
     sdec = base.decimal_to_sexagesimal(np.degrees(dec))
 
-    print 'SAPPHiRE:', '%d:%02d:%02.2f' % sra, '%d:%02d:%02.2f' % sdec
-    print 'SAPPHiRE: %10.6f %10.6f' % (ra, dec)
-    print 'SAPPHiRE: %10.6f %10.6f' % (np.degrees(ra), np.degrees(dec))
+#     print 'SAPPHiRE:', '%d:%02d:%02.2f' % sra, '%d:%02d:%02.2f' % sdec
+#     print 'SAPPHiRE: %10.6f %10.6f' % (ra, dec)
+    print 'SAPPHiRE:  %10.6f %10.6f' % (np.degrees(ra), np.degrees(dec))
 
 
 if __name__ == '__main__':
+    print 'Code base   right asc    declination'
     calc_ephem()
+    calc_astropy()
     calc_sapphire()
 
