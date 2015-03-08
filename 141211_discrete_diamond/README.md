432e21f701c4125e5c0c859132dc02c178bf49ca
Arne de Laat <adelaat@nikhef.nl>
2015-03-08 14:04:44 +0100

    Update discrete directions

---
+++ 141211_discrete_diamond/README.md
@@ -0,0 +1,14 @@
+Discrete directions
+===================
+
+The 2.5 ns sample rate of the detectors results in a set of discrete
+solutions for any three detectors in a station. Assuming two detectors
+have a fixed arrival time and the arrival time of the third varies an
+arc will be drawn through the possible directions.
+
+
+## discrete_directions_diamond.py
+
+Make discrete direction plots for each combination of 3 detectors in
+diamond-shaped station. All solutions are shown and several arcs are
+drawn for different detectors varying in time.
--- 141211_discrete_diamond/discrete_directions_diamond.py
+++ 141211_discrete_diamond/discrete_directions_diamond.py
@@ -11,7 +11,7 @@ from sapphire.analysis.direction_reconstruction import \
 
 TIME_RESOLUTION = 2.5  # nanoseconds
 C = .3  # lightspeed m/ns
-
+COLORS = ['black', 'red', 'green', 'blue']
 
 def generate_discrete_times(station, detector_ids=[0, 2, 3]):
     """Generates possible arrival times for detectors
@@ -42,15 +42,8 @@ def ceil_in_base(value, base):
     return base * np.ceil(value / base)
 
 
-if __name__ == '__main__':
-
-    dirrec = DirectAlgorithmCartesian3D()
-
-    station = SingleDiamondStation().stations[0]
-
+def reconstruct_for_detectors(ids):
     graph = PolarPlot(use_radians=True)
-
-    ids = [1, 2, 3]
     times = generate_discrete_times(station, detector_ids=ids)
     detectors = [station.detectors[id].get_coordinates() for id in ids]
     x, y, z = zip(*detectors)
@@ -66,22 +59,22 @@ if __name__ == '__main__':
     # and a straight line where detector 0 and 2 have equal times
     times = np.arange(-60, 60, TIME_RESOLUTION)
 
-    for dt in (0, 2.5, 5., 7.5, 10, 22.5):
+    for dt in (-2.5, 0, 2.5, 5., 7.5, 10, 22.5):
         theta, phi = itertools.izip(*(dirrec.reconstruct_common((t, 0, dt), x, y, z)
                                       for t in times))
         thetaa = [t for t in theta if not np.isnan(t)]
         phia = [p for p in phi if not np.isnan(p)]
-        graph.plot(phia, thetaa, mark=None, linestyle='red,solid')
+        graph.plot(phia, thetaa, mark=None, linestyle='solid,' + COLORS[ids[0]])
         theta, phi = itertools.izip(*(dirrec.reconstruct_common((0, t, dt), x, y, z)
                                       for t in times))
         thetaa = [t for t in theta if not np.isnan(t)]
         phia = [p for p in phi if not np.isnan(p)]
-        graph.plot(phia, thetaa, mark=None, linestyle='blue,solid')
+        graph.plot(phia, thetaa, mark=None, linestyle='solid,' + COLORS[ids[1]])
         theta, phi = itertools.izip(*(dirrec.reconstruct_common((0, dt, t), x, y, z)
                                       for t in times))
         thetaa = [t for t in theta if not np.isnan(t)]
         phia = [p for p in phi if not np.isnan(p)]
-        graph.plot(phia, thetaa, mark=None, linestyle='green,solid')
+        graph.plot(phia, thetaa, mark=None, linestyle='solid,' + COLORS[ids[2]])
 
     graph.set_ylimits(0, np.pi / 2)
     graph.set_yticks([0, np.pi / 6, np.pi / 3, np.pi / 2])
@@ -89,5 +82,16 @@ if __name__ == '__main__':
                            r'$\frac{2}{6}\pi$', r'$\frac{1}{2}\pi$',])
     graph.set_ylabel('Zenith [rad]')
     graph.set_xlabel('Azimuth [rad]')
-    graph.save_as_pdf('discrete_directions_diamond')
-    graph.save('discrete_directions_diamond')
+    graph.save_as_pdf('discrete_directions_%s' % '_'.join(str(i) for i in ids))
+
+
+if __name__ == '__main__':
+
+    dirrec = DirectAlgorithmCartesian3D()
+
+    station = SingleDiamondStation().stations[0]
+
+    reconstruct_for_detectors([0, 1, 2])
+    reconstruct_for_detectors([0, 1, 3])
+    reconstruct_for_detectors([0, 2, 3])
+    reconstruct_for_detectors([1, 2, 3])
