74fdd3bebf3a53fcb43bb0112be84146e8e01545
Arne de Laat <adelaat@nikhef.nl>
2015-03-12 00:01:16 +0100

    Add reconstructed core and direction to display.

--- 150220_reconstruction_display/coincidence_reconstruction_display.py
+++ 150220_reconstruction_display/coincidence_reconstruction_display.py
@@ -1,9 +1,10 @@
 import tables
+from math import cos, sin
 from numpy import nan_to_num, nanmin, isnan
 from artist import Plot, MultiPlot
 
 from sapphire.clusters import HiSPARCStations
-from sapphire.analysis import reconstructions, coincidence_queries, event_utils
+from sapphire.analysis import coincidence_queries, event_utils
 
 
 COIN_DATA = '/Users/arne/Datastore/esd_coincidences/coincidences_n7_120101_140801.h5'
@@ -18,7 +19,7 @@ OFFSETS =  {501: [-1.10338, 0.0000, 5.35711, 3.1686],
 DETECTOR_IDS = [0, 1, 2, 3]
 
 
-def display_coincidences(coincidence_events):
+def display_coincidences(coincidence_events, reconstruction, c_id):
     cluster = HiSPARCStations(range(501, 511))
 
     ts0 = coincidence_events[0][1]['ext_timestamp']
@@ -28,7 +29,7 @@ def display_coincidences(coincidence_events):
     t = []
     p = []
 
-    plot = Plot()
+    plot = Plot(width=r'.6\linewidth', height=r'.5\linewidth')
 
     for station_number, event in coincidence_events:
         if station_number == 507:
@@ -61,20 +62,35 @@ def display_coincidences(coincidence_events):
 
     plot.scatter_table(xx, yy, tt, pp)
 
+    dx = cos(reconstruction['azimuth'])
+    dy = sin(reconstruction['azimuth'])
+    direction_length = reconstruction['zenith'] * 200
+    core_x = reconstruction['x']
+    core_y = reconstruction['y']
+
+    plot.scatter([core_x], [core_y], mark='10-pointed star')
+    plot.plot([core_x, core_x + direction_length * dx],
+              [core_y, core_y + direction_length * dy], mark=None)
+
     plot.set_scalebar(location="lower left")
+    plot.set_slimits(min=1, max=60)
     plot.set_colorbar('$\Delta$t [\si{n\second}]')
     plot.set_axis_equal()
+    plot.set_xlimits(min=-250, max=350)
+    plot.set_ylimits(min=-250, max=250)
 
     plot.set_xlabel('x [\si{\meter}]')
     plot.set_ylabel('y [\si{\meter}]')
 
-    plot.save_as_pdf('event_display')
+    plot.save_as_pdf('event_display_%d' % c_id)
 
 
 if __name__ == '__main__':
     with tables.open_file(COIN_DATA, 'r') as data:
         cq = coincidence_queries.CoincidenceQuery(data)
-        coincidence = cq.coincidences[3]
-        coincidence_events = cq.events_from_stations([coincidence],
-                                                     range(501, 511)).next()
-        display_coincidences(coincidence_events)
+        for c_id in range(30):
+            coincidence = cq.coincidences[c_id]
+            coincidence_events = cq.events_from_stations([coincidence],
+                                                         range(501, 511)).next()
+            reconstruction = cq._get_reconstruction(coincidence)
+            display_coincidences(coincidence_events, reconstruction, c_id)
