5c5759c8ba6b4fa54bc97c94cd12b4c876636e2d
Arne de Laat <adelaat@nikhef.nl>
2015-04-17 16:10:43 +0200

    Update script to determine detector offset distribution
    Determine the distribution for multiple dates.

--- 140804_detector_offsets/detector_offsets.py
+++ 140804_detector_offsets/detector_offsets.py
@@ -1,4 +1,4 @@
-""" Determine detector offset distrobution
+""" Determine detector offset distribution
 
 This determines the detector offsets for all stations in an ESD datafile.
 These offsets are then fitted and the results are shown.
@@ -16,14 +16,17 @@ from sapphire.analysis.reconstructions import ReconstructESDCoincidences
 from sapphire.utils import gauss
 from sapphire.clusters import Station
 
+DATA_PATH = '/Users/arne/Datastore/esd/2014/1/'
+BIN_WIDTH = 2.5
+
 
 def determine_offset(dirrec, s_path):
-    station_number = int(s_path.split('station_')[-1])
     station_group = dirrec.data.get_node(s_path)
     o = (0, 0, 0)
     station = Station(None, 0, o,
                       detectors=[(o, 'UD'), (o, 'UD'), (o, 'LR'), (o, 'LR')])
-    offsets = [offset for offset in dirrec.determine_detector_timing_offsets(station, station_group)
+    offsets = [offset
+               for offset in dirrec.determine_detector_timing_offsets(station, station_group)
                if offset != 0.0]
     return offsets
 
@@ -37,7 +40,7 @@ def determine_offsets(data):
 
 
 def fit_offsets(offsets):
-    bins = arange(-40 + 1.25, 40, 2.5)
+    bins = arange(-40 + BIN_WIDTH / 2, 40, BIN_WIDTH)
     y, bins = histogram(offsets, bins=bins)
     x = (bins[:-1] + bins[1:]) / 2
     popt, pcov = curve_fit(gauss, x, y, p0=(len(offsets), 0., 2.5))
@@ -45,22 +48,24 @@ def fit_offsets(offsets):
 
 
 def plot_fit(x, y, popt, graph):
-    graph.plot(x - 1.25, y, mark=None, use_steps=True)
+    graph.plot(x - BIN_WIDTH / 2, y, mark=None, use_steps=True)
     fit_x = arange(min(x), max(x), 0.1)
     graph.plot(fit_x, gauss(fit_x, *popt), mark=None, linestyle='gray')
 
 
 if __name__ == '__main__':
-    graph = Plot()
 
-    data_path = '/Users/arne/Datastore/esd/2014/1/2014_1_1.h5'
-    with tables.open_file(data_path, 'r') as data:
-        offsets = determine_offsets(data)
-    x, y, popt = fit_offsets(offsets)
-    print 'sigma: ', popt[2]
-    print 'mean: ', popt[1]
-    plot_fit(x, y, popt, graph)
+    files = ['2014_1_1.h5', '2014_1_2.h5', '2014_1_3.h5', '2014_1_4.h5',
+             '2014_1_10.h5', '2014_1_20.h5', '2014_1_30.h5']
+    for file in files:
+        with tables.open_file(DATA_PATH + file, 'r') as data:
+            offsets = determine_offsets(data)
 
-    graph.set_ylabel('P')
-    graph.set_xlabel('$\Delta t$ [ns]')
-    graph.save_as_pdf('detector_offset_distribution')
+        graph = Plot()
+        x, y, popt = fit_offsets(offsets)
+        plot_fit(x, y, popt, graph)
+        graph.set_label('$\mu$: %f, $\sigma$: %f' % (popt[1], popt[2]))
+        graph.set_ylabel('P')
+        graph.set_xlabel('$\Delta t$ [ns]')
+        graph.set_ylimits(min=0)
+        graph.save_as_pdf('detector_offset_distribution_' + file[:-3])
