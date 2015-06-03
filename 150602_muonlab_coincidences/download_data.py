69a21855c6fd43dec54e310766fa4e536e031f93
Arne de Laat <adelaat@nikhef.nl>
2015-06-03 13:26:01 +0200

    Update muonlab download script
    Reduce data downloaded for first test.
    Add second test.

--- 150602_muonlab_coincidences/download_data.py
+++ 150602_muonlab_coincidences/download_data.py
@@ -10,11 +10,24 @@ DATA_PATH = '/Users/arne/Datastore/'
 
 
 def download_data(station):
-    """Download data from first day of each month"""
+    """Download data for the mounlab test
+
+    - First the test with the lower detector placed in the HiSPARC office
+    - Second the test with the lower detector placed in the Nikhef courtyard
+
+    """
     path = os.path.join(DATA_PATH, 'muonlab_test.h5')
     with open_file(path, 'a') as data:
+        # Two ranges since the muonlab station was offline part of the time
         esd.download_data(data, '/station_%d' % station, station,
-                          datetime(2015, 5, 28), datetime(2015, 6, 2))
+                          datetime(2015, 5, 28, 15), datetime(2015, 5, 29, 9))
+        esd.download_data(data, '/station_%d' % station, station,
+                          datetime(2015, 6, 1, 8), datetime(2015, 6, 2, 11))
+
+    path = os.path.join(DATA_PATH, 'muonlab_test2.h5')
+    with open_file(path, 'a') as data:
+        esd.download_data(data, '/station_%d' % station, station,
+                          datetime(2015, 6, 2, 12), datetime(2015, 6, 9))
 
 
 if __name__ == "__main__":
