dff1df1e74f7e2aeed4a0bcf18955270769f2af5
Arne de Laat <adelaat@nikhef.nl>
2015-06-29 16:21:57 +0200

    Check which simulations did not complete
    Some simulations failed/got stuck for unknown reasons.
    The cause is being investigated.
    This script checks which ones did not succeed.

---
+++ 150624_energy_simulations/check_finished.py
@@ -0,0 +1,18 @@
+import glob
+import os
+
+from numpy import log10, degrees
+import tables
+
+
+if __name__ == "__main__":
+    print "Simulations that did not complete:"
+
+    with tables.open_file('/Users/arne/Datastore/CORSIKA/corsika_overview_150624.h5', 'r') as overview:
+        for path in glob.glob('*/result.h5'):
+            seeds = [int(x) for x in os.path.dirname(path).split('_')]
+            simulation = overview.root.simulations.read_where('(seed1 == %d) & (seed2 == %d)' % tuple(seeds))
+            with tables.open_file(path, 'r') as data:
+                if data.root.coincidences.coincidences.nrows != 100000:
+                    print '10**%.1f eV, %.1f deg' % (log10(simulation['energy']), degrees(simulation['zenith']))
+                    print '%d_%d' % tuple(seeds), 'N =', data.root.coincidences.coincidences.nrows
