b70c2909a0a36fd8c0149705f677a680dc34a275
Arne de Laat <adelaat@nikhef.nl>
2015-07-03 19:13:15 +0200

    Reconstruct simulated coincidences

---
+++ 150624_energy_simulations/reconstruct.py
@@ -0,0 +1,39 @@
+""" Reconstruct the results
+
+"""
+import glob
+import os
+
+import tables
+
+from sapphire import ReconstructESDEvents, ReconstructESDCoincidences
+
+
+RESULT_PATH = '/Users/arne/Datastore/science_park_corsika/{seed}/result.h5'
+PATHS = '/Users/arne/Datastore/science_park_corsika/*_*/result.h5'
+
+
+def reconstruct_simulations(seed):
+    path = RESULT_PATH.format(seed=seed)
+    with tables.open_file(path, 'a') as data:
+        cluster = data.root.coincidences._v_attrs.cluster
+
+        rec_coins = ReconstructESDCoincidences(data, '/coincidences',
+                                               overwrite=True, progress=True)
+        rec_coins.prepare_output()
+        rec_coins.offsets = {station.number: [d.offset + station.gps_offset
+                                              for d in station.detectors]
+                             for station in cluster.stations}
+        try:
+            rec_coins.reconstruct_directions()
+            rec_coins.reconstruct_cores()
+            rec_coins.store_reconstructions()
+        except:
+            pass
+
+
+if __name__ == '__main__':
+    for path in glob.glob(PATHS):
+        seed = os.path.basename(os.path.dirname(path))
+        print seed
+        reconstruct_simulations(seed)
