a25439c09ac659bc6577251ece9cac47f67f5f4c
Arne de Laat <adelaat@nikhef.nl>
2015-07-22 15:46:36 +0200

    Plot for first interaction altitude, similar to shower sizes
    To be improved by comparing different primary particle kinds.

--- 150722_interaction_altitudes/interaction_altitudes.py
+++ 150722_interaction_altitudes/interaction_altitudes.py
@@ -8,14 +8,14 @@ from numpy import log10, median, percentile
 OVERVIEW = '/Users/arne/Datastore/CORSIKA/corsika_overview.h5'
 
 
-def plot_shower_size():
+def plot_interaction_height():
     plot = Plot(axis='semilogy')
     with tables.open_file(OVERVIEW, 'r') as overview:
         sims = overview.get_node('/simulations')
         for e in set(log10(sims.col('energy'))):
-            median_size = []
-            min_size = []
-            max_size = []
+            median_altitude = []
+            min_altitude = []
+            max_altitude = []
             zeniths = sorted(set(sims.read_where('(log10(energy) == e) & '
                                                  '(particle_id == 14)',
                                                  field='zenith')))
@@ -23,18 +23,19 @@ def plot_shower_size():
                 selection = sims.read_where('(log10(energy) == e) & '
                                             '(zenith == z) & '
                                             '(particle_id == 14)')
-                n_leptons = selection['n_electron'] + selection['n_muon']
-                median_size.append(median(n_leptons))
-                min_size.append(percentile(n_leptons, 16))
-                max_size.append(percentile(n_leptons, 84))
+                interaction_altitudes = selection['first_interaction_altitude']
+                median_altitude.append(median(interaction_altitudes))
+                min_altitude.append(percentile(interaction_altitudes, 16))
+                max_altitude.append(percentile(interaction_altitudes, 84))
             if len(zeniths):
-                plot.plot(zeniths, median_size)
-                plot.shade_region(zeniths, min_size, max_size, color='lightgray,semitransparent')
+                plot.plot(zeniths, median_altitude)
+                plot.shade_region(zeniths, min_altitude, max_altitude,
+                                  color='lightgray,semitransparent')
                 plot.add_pin('%.1f' % e, relative_position=0)
-    plot.set_ylabel(r'Shower size (leptons)')
+    plot.set_ylabel(r'First interaction altitude [m]')
     plot.set_xlabel(r'Zenith [\si{\radian}]')
-    plot.save_as_pdf('shower_sizes')
+    plot.save_as_pdf('interaction_altitude')
 
 
 if __name__ == "__main__":
-    plot_shower_size()
+    plot_interaction_height()
