fc9009ff0f1acbe7768ca6de0fe5fb56e6c07244
Arne de Laat <adelaat@nikhef.nl>
2015-11-18 15:23:40 +0100

    Update shower size plots
    Use degrees as units for x-axis and fix ticks at 7.5° intervals.
    Fix y axis limits.
    Make separate plots for muons, electrons and both.

--- 150702_shower_sizes/shower_sizes.py
+++ 150702_shower_sizes/shower_sizes.py
@@ -1,6 +1,6 @@
 from artist import Plot
 import tables
-from numpy import log10, median, percentile
+from numpy import log10, median, percentile, degrees, array, arange
 
 # proton = 14
 # iron = 5626
@@ -8,8 +8,8 @@ from numpy import log10, median, percentile
 OVERVIEW = '/Users/arne/Datastore/CORSIKA/corsika_overview.h5'
 
 
-def plot_shower_size():
     plot = Plot(axis='semilogy')
+def plot_shower_size(leptons=['electron', 'muon']):
     with tables.open_file(OVERVIEW, 'r') as overview:
         sims = overview.get_node('/simulations')
         for e in set(log10(sims.col('energy'))):
@@ -23,18 +23,24 @@ def plot_shower_size():
                 selection = sims.read_where('(log10(energy) == e) & '
                                             '(zenith == z) & '
                                             '(particle_id == 14)')
-                n_leptons = selection['n_electron'] + selection['n_muon']
+                n_leptons = selection['n_%s' % leptons[0]]
+                for lepton in leptons[1:]:
+                    n_leptons += selection['n_%s' % lepton]
                 median_size.append(median(n_leptons))
                 min_size.append(percentile(n_leptons, 16))
                 max_size.append(percentile(n_leptons, 84))
             if len(zeniths):
-                plot.plot(zeniths, median_size)
-                plot.shade_region(zeniths, min_size, max_size, color='lightgray,semitransparent')
+                plot.plot(degrees(array(zeniths)), median_size, linestyle='very thin')
+                plot.shade_region(degrees(array(zeniths)), min_size, max_size, color='lightgray,semitransparent')
                 plot.add_pin('%.1f' % e, relative_position=0)
+    plot.set_xticks([t for t in arange(0, 60.1, 7.5)])
+    plot.set_ylimits(1, 1e9)
     plot.set_ylabel(r'Shower size (leptons)')
-    plot.set_xlabel(r'Zenith [\si{\radian}]')
-    plot.save_as_pdf('shower_sizes')
+    plot.set_xlabel(r'Zenith [\si{\degree}]')
+    plot.save_as_pdf('shower_sizes_%s' % '_'.join(leptons))
 
 
 if __name__ == "__main__":
     plot_shower_size()
+    plot_shower_size(['muon'])
+    plot_shower_size(['electron'])
