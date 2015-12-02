9d70724e350cc9db3d70d496627456113b85b09d
Arne de Laat <adelaat@nikhef.nl>
2015-12-02 16:18:10 +0100

    Add option to plot raw traces and show trigger windows
    Only show trigger windows if data reduction is off.
    todo: use API to check data reduction status.

--- 151104_event_trace/plot_trace.py
+++ 151104_event_trace/plot_trace.py
@@ -19,7 +19,7 @@ from artist import Plot
 from sapphire import Station
 
 
-def get_traces(station_number, ts, ns):
+def get_traces(station_number, ts, ns, raw=False):
     """Retrieve the traces
 
     :param station_number: number of the station to which the event belongs.
@@ -28,11 +28,11 @@ def get_traces(station_number, ts, ns):
     :return: the traces.
 
     """
-    traces = Station(station_number).event_trace(ts, ns, raw=False)
+    traces = Station(station_number).event_trace(ts, ns, raw)
     return traces
 
 
-def plot_traces(traces, label=''):
+def plot_traces(traces, label='', raw=False):
     """Plot traces
 
     :param traces: list of lists of trace values.
@@ -40,14 +40,24 @@ def plot_traces(traces, label=''):
 
     """
     colors = ['black', 'red', 'black!40!green', 'blue']
-    plot = Plot(width=r'.5\textwidth')
+    plot = Plot(width=r'1.\textwidth')
     times = arange(0, len(traces[0]) * 2.5, 2.5)
     for i, trace in enumerate(traces):
-        plot.plot(times, [t * -0.57 / 1e3 for t in trace],
-                  linestyle='%s, thick' % colors[i], mark=None)
-    plot.set_xlimits(0, times[-1])
-    plot.set_ylabel(r'Signal strength [V]')
+        if not raw:
+            trace = [t * -0.57 / 1e3 for t in trace]
+        plot.plot(times, trace, linestyle='%s, thin' % colors[i], mark=None)
+    if len(traces[0]) == 2400 and raw:
+        plot.add_pin_at_xy(500, 10, 'pre-trigger', location='below', use_arrow=False)
+        plot.add_pin_at_xy(1750, 10, 'trigger', location='below', use_arrow=False)
+        plot.add_pin_at_xy(4250, 10, 'post-trigger', location='below', use_arrow=False)
+        plot.draw_vertical_line(1000, 'gray')
+        plot.draw_vertical_line(2500, 'gray')
+    if raw:
+        plot.set_ylabel(r'Signal strength [ADCcounts]')
+    else:
+        plot.set_ylabel(r'Signal strength [\si{\volt}]')
     plot.set_xlabel(r'Event time [\si{\ns}]')
+    plot.set_xlimits(0, times[-1])
     plot.set_axis_options('line join=round')
     plot.save_as_pdf('trace_' + label)
 
@@ -57,11 +67,12 @@ def main():
     parser.add_argument('number', type=int, help="Station number")
     parser.add_argument('timestamp', type=int, help="Timestamp of the event")
     parser.add_argument('nanoseconds', type=int, help="Nanosecond part of the timestamp")
+    parser.add_argument('--raw', action='store_true', help="Get raw trace values")
     args = parser.parse_args()
 
     label = '%d_%d_%d' % (args.number, args.timestamp, args.nanoseconds)
-    traces = get_traces(args.number, args.timestamp, args.nanoseconds)
-    plot_traces(traces, label)
+    traces = get_traces(args.number, args.timestamp, args.nanoseconds, args.raw)
+    plot_traces(traces, label, args.raw)
 
 
 if __name__ == "__main__":
