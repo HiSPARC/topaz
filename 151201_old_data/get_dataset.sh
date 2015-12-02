77a3c8ecdf7950e9fc68a1cc31feffef43118a80
Arne de Laat <adelaat@nikhef.nl>
2015-12-02 16:12:30 +0100

    Commands to download some old datafiles

---
+++ 151201_old_data/get_dataset.sh
@@ -0,0 +1,3 @@
+scp hisparc@pique.nikhef.nl:/databases/frome/2004/7/2004_7_1.h5 ~/Datastore/old_data/
+scp hisparc@pique.nikhef.nl:/databases/frome/2005/7/2005_7_1.h5 ~/Datastore/old_data/
+scp hisparc@pique.nikhef.nl:/databases/frome/2006/7/2006_7_1.h5 ~/Datastore/old_data/
