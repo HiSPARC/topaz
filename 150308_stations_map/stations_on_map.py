cf1e20fb452843c6be86d2182e39f7593ba1205f
Arne de Laat <adelaat@nikhef.nl>
2015-12-09 12:35:27 +0100

    Improve stations on map
    Allow plotting of all KNMI and/or HiSPARC weather stations.

--- 150308_stations_map/stations_on_map.py
+++ 150308_stations_map/stations_on_map.py
@@ -51,11 +51,41 @@ def get_station_locations(country=None, cluster=None, subcluster=None,
             continue
         latitudes.append(location['latitude'])
         longitudes.append(location['longitude'])
+
+    return latitudes, longitudes
+
+def get_weather_locations():
+    latitudes = []
+    longitudes = []
+
+    station_numbers = [s['number']
+                       for s in Network().stations_with_weather()]
+
+    for station_number in station_numbers:
+        location = Station(station_number).location()
+        latitudes.append(location['latitude'])
+        longitudes.append(location['longitude'])
+
+    return latitudes, longitudes
+
+
+def get_knmi_locations():
+    latitudes = [52.165, 52.463, 52.924, 52.301, 53.255, 52.644, 53.393,
+                 52.506, 52.101, 52.130, 52.896, 52.458, 53.225, 52.703,
+                 52.061, 53.409, 52.437, 52.750, 53.125, 52.073, 53.196,
+                 52.273, 51.442, 51.226, 51.527, 51.993, 51.448, 51.955,
+                 51.972, 51.568, 51.858, 51.446, 51.657, 51.197, 50.910,
+                 51.498]
+    longitudes = [4.419, 4.575, 4.785, 4.774, 4.942, 4.979, 5.346, 4.603,
+                  5.177, 5.274, 5.384, 5.526, 5.755, 5.889, 5.888, 6.196,
+                  6.263, 6.575, 6.586, 6.650, 7.150, 6.897, 3.596, 3.862,
+                  3.884, 4.124, 4.349, 4.444, 4.927, 4.933, 5.145, 5.414,
+                  5.706, 5.764, 5.768, 6.196]
     return latitudes, longitudes
 
 
 def make_map(country=None, cluster=None, subcluster=None, station=None,
-             label='map', detectors=False):
+             label='map', detectors=False, weather=False, knmi=False):
 
     if detectors:
         latitudes, longitudes = get_detector_locations(country, cluster,
@@ -64,8 +94,23 @@ def make_map(country=None, cluster=None, subcluster=None, station=None,
         latitudes, longitudes = get_station_locations(country, cluster,
                                                       subcluster, station)
 
-    map = Map((min(latitudes), min(longitudes),
-               max(latitudes), max(longitudes)), margin=.1)
+    if weather:
+        weather_latitudes, weather_longitudes = get_weather_locations()
+    else:
+        weather_latitudes, weather_longitudes = ([], [])
+
+    if knmi:
+        knmi_latitudes, knmi_longitudes = get_knmi_locations()
+    else:
+        knmi_latitudes, knmi_longitudes = ([], [])
+
+
+    bounds = (min(latitudes + weather_latitudes + knmi_latitudes),
+              min(longitudes + weather_longitudes + knmi_longitudes),
+              max(latitudes + weather_latitudes + knmi_latitudes),
+              max(longitudes + weather_longitudes + knmi_longitudes))
+
+    map = Map(bounds, margin=.1)
     map.save_png('map-tiles-background.png')
     image = map.to_pil()
 
@@ -74,32 +119,39 @@ def make_map(country=None, cluster=None, subcluster=None, station=None,
 
     width = 0.67
     height = width / aspect
-    graph = Plot(width=r'%.2f\linewidth' % width,
+    plot = Plot(width=r'%.2f\linewidth' % width,
                  height=r'%.2f\linewidth' % height)
 
-    graph.draw_image(image, 0, 0, map_w, map_h)
-    graph.set_axis_equal()
+    plot.draw_image(image, 0, 0, map_w, map_h)
+    plot.set_axis_equal()
 
     xmin, ymin = map.to_pixels(map.box[:2])
     xmax, ymax = map.to_pixels(map.box[2:])
-    graph.set_xlimits(xmin, xmax)
-    graph.set_ylimits(map_h - ymin, map_h - ymax)
+    plot.set_xlimits(xmin, xmax)
+    plot.set_ylimits(map_h - ymin, map_h - ymax)
 
     x, y = map.to_pixels(array(latitudes), array(longitudes))
-    graph.scatter(x, map_h - y, markstyle="black!50!green, thick")
+    plot.scatter(x, map_h - y, markstyle="black!50!green, thick")
 
-    graph.set_xlabel('Longitude [$^\circ$]')
-#     graph.set_xticks([0, map_w])
-#     graph.set_xtick_labels([nw[1], se[1]])
+    if weather:
+        x, y = map.to_pixels(array(weather_latitudes), array(weather_longitudes))
+        plot.scatter(x, map_h - y, markstyle="black!50!red, thick")
+    if knmi:
+        x, y = map.to_pixels(array(knmi_latitudes), array(knmi_longitudes))
+        plot.scatter(x, map_h - y, markstyle="black!50!blue, thick")
 
-    graph.set_ylabel('Latitude [$^\circ$]')
-#     graph.set_yticks([0, map_h])
-#     graph.set_ytick_labels([se[0], nw[0]])
+    plot.set_xlabel('Longitude [$^\circ$]')
+#     plot.set_xticks([0, map_w])
+#     plot.set_xtick_labels([nw[1], se[1]])
 
-#     graph.set_title(label)
+    plot.set_ylabel('Latitude [$^\circ$]')
+#     plot.set_yticks([0, map_h])
+#     plot.set_ytick_labels([se[0], nw[0]])
 
-    # save graph to file
-    graph.save_as_pdf(label.replace(' ', '-'))
+#     plot.set_title(label)
+
+    # save plot to file
+    plot.save_as_pdf(label.replace(' ', '-'))
 
 
 def main():
@@ -118,28 +170,27 @@ def main():
                         help='Number represents the station')
     parser.add_argument('--detectors', action='store_true',
                         help='Show each detector')
+    parser.add_argument('--weather', action='store_true',
+                        help='Show all HiSPARC weather stations')
+    parser.add_argument('--knmi', action='store_true',
+                        help='Show all KNMI weather stations')
     args = parser.parse_args()
-
-    if args.detectors:
-        detectors = True
-    else:
-        detectors = False
 
     if args.network:
         label = 'network'
-        make_map(label=label, detectors=detectors)
+        make_map(label=label, detectors=args.detectors, weather=args.weather, knmi=args.knmi)
     elif args.country:
-        label = 'country %d' % args.number
-        make_map(country=args.number, label=label, detectors=detectors)
+        label = 'country_%d' % args.number
+        make_map(country=args.number, label=label, detectors=args.detectors, weather=args.weather, knmi=args.knmi)
     elif args.cluster:
-        label = 'cluster %d' % args.number
-        make_map(cluster=args.number, label=label, detectors=detectors)
+        label = 'cluster_%d' % args.number
+        make_map(cluster=args.number, label=label, detectors=args.detectors, weather=args.weather, knmi=args.knmi)
     elif args.subcluster:
-        label = 'subcluster %d' % args.number
-        make_map(subcluster=args.number, label=label, detectors=detectors)
+        label = 'subcluster_%d' % args.number
+        make_map(subcluster=args.number, label=label, detectors=args.detectors, weather=args.weather, knmi=args.knmi)
     elif args.station:
-        label = 'station %d' % args.number
-        make_map(station=args.number, label=label, detectors=detectors)
+        label = 'station_%d' % args.number
+        make_map(station=args.number, label=label, detectors=args.detectors, weather=args.weather, knmi=args.knmi)
 
 
 if __name__ == '__main__':
