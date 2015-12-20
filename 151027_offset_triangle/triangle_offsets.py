0f68ba6f8a041ece04a97585297295519257be6b
Arne de Laat <adelaat@nikhef.nl>
2015-12-20 16:18:26 +0100

    Use artist for offset distributions when round tripping.

--- 151027_offset_triangle/triangle_offsets.py
+++ 151027_offset_triangle/triangle_offsets.py
@@ -2,34 +2,44 @@
 
 from itertools import combinations, permutations
 
+from numpy import histogram, histogram2d, isnan, array
+
+from artist import Plot
+
 from get_aligned_offsets import get_data
-
-
-offsets = get_data()
 
 
 def round_trip(offsets):
     stations = offsets.keys()
-    for n in [1, 2, 3, 4, 5]:
-        figure()
+    idx = range(len(offsets[stations[0]][stations[1]]))
+    for n in [2, 3, 4, 5]:
+        plot = Plot()
+        ts = []
+        offs = []
         for ref in stations:
             for s in combinations(stations, n):
                 if ref in s:
                     continue
-                plot(offsets[ref][s[0]] +
-                     sum(offsets[s[i]][s[i+1]] for i in range(n-1)) +
-                     offsets[s[-1]][ref])
-        ylim(-100, 100)
-        title('n = %d' % n)
+                offs.extend(offsets[ref][s[0]] +
+                            sum(offsets[s[i]][s[i+1]] for i in range(n-1)) +
+                            offsets[s[-1]][ref])
+                ts.extend(idx)
+        ts = array(ts)
+        offs = array(offs)
+        ts = ts.compress(~isnan(offs))
+        offs = offs.compress(~isnan(offs))
+        counts, xedges, yedges = histogram2d(ts, offs, bins=(idx[::4], range(-100, 100, 5)))
+        plot.histogram2d(counts, xedges, yedges, bitmap=True, type='color')
+        plot.set_ylimits(-100, 100)
+        plot.set_title('n = %d' % n)
+        plot.save_as_pdf('round_trip_%d' % n)
 
 
 def offset_distribution(offsets):
     stations = offsets.keys()
-    idx = range(len(offsets[ref][s[0]])
     for n in [1, 2, 3, 4, 5]:
-        ts = []
+        plot = Plot()
         offs = []
-        figure()
         for ref in stations:
             for s in permutations(stations, n):
                 if ref in s:
@@ -37,13 +47,14 @@ def offset_distribution(offsets):
                 offs.extend(offsets[ref][s[0]] +
                             sum(offsets[s[i]][s[i+1]] for i in range(n-1)) +
                             offsets[s[-1]][ref])
-                ts.extend(idx)
-        hist(offs, bins=range(-100, 100, 2))
-        xlim(-100, 100)
-        title('n = %d' % n)
+        plot.histogram(*histogram(offs, bins=range(-100, 100, 2)))
+        plot.set_xlimits(-100, 100)
+        plot.set_ylimits(min=0)
+        plot.set_title('n = %d' % n)
+        plot.save_as_pdf('round_trip_dist_%d' % n)
 
 
 if __name__ == "__main__":
     offsets = get_data()
-    round_trip(offets)
+    round_trip(offsets)
     offset_distribution(offsets)
