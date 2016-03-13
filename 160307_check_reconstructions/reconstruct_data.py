b162412bfdf8177dbd00eb10d5d7cb3d5aa7c5d1
Arne de Laat <adelaat@nikhef.nl>
2016-03-13 23:25:35 +0100

    Reconstruct coincidences using data from each detector

--- 160307_check_reconstructions/reconstruct_data.py
+++ 160307_check_reconstructions/reconstruct_data.py
@@ -3,6 +3,8 @@ import os.path
 import tables
 
 from sapphire import ReconstructESDCoincidences
+from sapphire.analysis.direction_reconstruction import (
+    CoincidenceDirectionReconstructionDetectors)
 
 DATASTORE = "/Users/arne/Datastore/check_reconstructions"
 
@@ -12,7 +14,15 @@ def reconstruct_data(data):
     rec.reconstruct_and_store()
 
 
+def reconstruct_data_using_detectors(data):
+    rec_d = ReconstructESDCoincidences(data, overwrite=True,
+                                       destination='reconstructions_detectors')
+    rec_d.direction = CoincidenceDirectionReconstructionDetectors(rec_d.cluster)
+    rec_d.reconstruct_and_store()
+
+
 if __name__ == '__main__':
     path = os.path.join(DATASTORE, 'dataset_sciencepark_n10_151101_160201.h5')
     with tables.open_file(path, 'a') as data:
         reconstruct_data(data)
+        reconstruct_data_using_detectors(data)
