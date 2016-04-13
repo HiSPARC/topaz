e6435e606ad34fada10e9649a6f32001513a4327
Arne de Laat <adelaat@nikhef.nl>
2016-04-13 14:00:23 +0200

    Add Ciampa convolved with Guassian for zenith fit
    Add plot on sec(theta) axis.
    Update docstrings.

--- 160215_zenith_distribution/zenith_distributions.py
+++ 160215_zenith_distribution/zenith_distributions.py
@@ -1,14 +1,16 @@
 """Compare station zenith distribution to fits"""
 
 from __future__ import division
+from functools import partial
 
 from scipy.stats import binned_statistic
 from scipy.optimize import curve_fit
-from numpy import std, sin, cos, exp, arange, array, radians
+from numpy import std, sin, cos, exp, arange, array, radians, interp, sum
 
 from artist import Plot
 
 from sapphire import Station
+from sapphire.utils import gauss
 
 
 def get_zenith_distribution():
@@ -42,15 +44,22 @@ def get_zenith_distribution():
 
 
 def Rossi(x, A, B):
-    """ Rossi: zenith angle distribution """
+    """ Rossi: zenith angle distribution
 
+    :param x: zenith in degrees.
+
+    """
     rx = radians(x)
     geometry = sin(rx) * cos(rx)
     return A * geometry * cos(rx) ** B
 
 
 def Iyono(x, A, B):
-    """ Iyono 2007: zenith angle distribution """
+    """ Iyono 2007: zenith angle distribution
+
+    :param x: zenith in degrees.
+
+    """
 
     rx = radians(x)
     geometry = sin(rx) * cos(rx)
@@ -58,8 +67,11 @@ def Iyono(x, A, B):
 
 
 def Ciampa(x, A, C, D):
-    """ Ciampa 1998: zenith angle distribution """
+    """ Ciampa 1998: zenith angle distribution
 
+    :param x: zenith in degrees.
+
+    """
     rx = radians(x)
     geometry = sin(rx) * cos(rx)
     return A * geometry * exp(C * (1 / cos(rx)) - D)
@@ -67,6 +79,8 @@ def Ciampa(x, A, C, D):
 
 def ModCiampa(x, A, C):
     """ Based on Ciampa 1998: zenith angle distribution
+
+    :param x: zenith in degrees.
 
     Positive C parameter
 
@@ -76,18 +90,64 @@ def ModCiampa(x, A, C):
     return A * geometry * exp(-C * (1 / cos(rx)))
 
 
+def ModCiampa_conv_full(A, C, E):
+    """ Based on Ciampa 1998: zenith angle distribution
+
+    Positive C parameter
+
+    """
+    x = arange(0.1, 85, 0.1)
+    rx = radians(x)
+    result = sum([gauss(rx, ModCiampa(xi, A, C), rxi, radians(E / cos(rxi)))
+                  for xi, rxi in zip(x, rx)], axis=0)
+    return rx, result
+
+
+def ModCiampa_conv(x, A, C, E):
+    """ Based on Ciampa 1998: zenith angle distribution
+
+    :param x: zenith in degrees.
+
+    Positive C parameter
+
+    """
+    rx, result = ModCiampa_conv_full(A, C, E)
+    return interp(radians(x), rx, result)
+
+
 def plot_zenith(angle_centers, mean_counts, std_counts):
-    fit_functions = [Rossi, Iyono, ModCiampa]
+    fit_functions = [Rossi, Iyono, ModCiampa, ModCiampa_conv]
+
+    rangle_centers = radians(angle_centers)
+    sangle_centers = convert_angles(angle_centers)
+    angles = arange(0.1, 85, 0.1)
+    rangles = radians(angles)
+
     for fit_function in fit_functions:
         popt, pcov = curve_fit(fit_function, angle_centers, mean_counts) #, sigma=std_counts)
         print popt
+
         plot = Plot()
-        plot.scatter(angle_centers, mean_counts, yerr=std_counts)
-        angles = arange(0, 90, 0.1)
+        plot.scatter(angle_centers, mean_counts, yerr=std_counts, markstyle='red')
         plot.plot(angles, fit_function(angles, *popt), mark=None)
         plot.set_ylabel('Counts')
         plot.set_xlabel(r'Zenith [\si{\degree}]')
         plot.save_as_pdf('zenith_distribution_%s' % fit_function.func_name)
+
+        plot = Plot('semilogy')
+        plot.scatter(sangle_centers, mean_counts / sin(rangle_centers), yerr=std_counts)
+        fitted_counts = fit_function(angles, *popt)
+        plot.plot(convert_angles(angles), fitted_counts / sin(rangles), mark=None)
+        plot.plot(convert_angles(angles), fitted_counts, linestyle='gray', mark=None)
+        plot.set_ylabel('Counts')
+        plot.set_xlimits(0, 1)
+        plot.set_xlabel(r'Zenith angle [$\sec \theta - 1$]')
+        plot.save_as_pdf('zenith_distribution_sec_%s' % fit_function.func_name)
+
+
+
+def convert_angles(angles):
+    return 1. / cos(radians(angles)) - 1
 
 
 if __name__ == "__main__":
