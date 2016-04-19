b4bb1791adad5b4261ff8ad09669fd04455f2ab2
Arne de Laat <adelaat@nikhef.nl>
2016-04-19 16:04:02 +0200

    Improve overview of available CORSIKA simulations

--- 141124_corsika_overview/corsika_overview.py
+++ 141124_corsika_overview/corsika_overview.py
@@ -5,12 +5,14 @@ Get values from corsika_overview.h5
 
 """
 from random import choice
+from itertools import cycle
 
 import tables
 import numpy
 import artist
 
 from sapphire.corsika.particles import name
+from sapphire import CorsikaQuery
 
 
 OVERVIEW = '/Users/arne/Datastore/CORSIKA/corsika_overview.h5'
@@ -74,82 +76,77 @@ def plot_azimuth(azimuth):
     counts, bins = numpy.histogram(azimuth, bins=bins)
     graph = artist.Plot()
     graph.plot(bins[:-1], counts)
-    graph.set_xlabel('Azimuth [rad]')
+    graph.set_xlabel(r'Azimuth [\si{\radian}]')
     graph.set_ylabel('Count')
     graph.set_ylimits(min=0)
     graph.set_title('Number of simulations per azimuth angle')
     graph.save_as_pdf('azimuth')
 
 
-def plot_energy_zenith(energy, zenith, particle_id=None):
+def plot_energy_zenith(cq, particle=None):
     """Energy, Zenith"""
 
-    e_bins = get_unique(energy)
-    e_bins.append(e_bins[-1] + .5)
-    z_bins = get_unique(zenith)
-    z_bins.append(z_bins[-1] + (z_bins[-1] - z_bins[-2]))
-    counts, e_bins, z_bins = numpy.histogram2d(energy, zenith, bins=[e_bins, z_bins])
+    e_bins = numpy.arange(11, 18.6, 0.5) - 0.25
+#     e_bins = sorted(cq.available_parameters('energy', particle=particle))
+#     e_bins.append(e_bins[-1] + 0.5)
+    z_bins = numpy.arange(0, 67.6, 7.5) - 3.75
+#     z_bins = sorted(cq.available_parameters('zenith', particle=particle))
+#     z_bins.append(z_bins[-1] + 7.5)
+
+    simulations = cq.simulations(particle=particle)
+    energy = numpy.log10(simulations['energy'])
+    zenith = numpy.degrees(simulations['zenith'])
+
+    counts, e_bins, z_bins = numpy.histogram2d(energy, zenith,
+                                               bins=[e_bins, z_bins])
     graph = artist.Plot() # axis='semilogx'
-    graph.histogram2d(numpy.sqrt(counts), e_bins - 0.25, z_bins - (z_bins[1] / 2), type='area')
+    graph.histogram2d(numpy.sqrt(counts), e_bins, z_bins,
+                      type='area')
     graph.set_xlimits(min=10, max=19)
-    graph.set_xlabel(r'Energy [\si{electronvolt}]')
-    graph.set_ylabel(r'Zenith [\si{\radian}]')
+    graph.set_xlabel(r'Energy [Log10 \si{\eV}]')
+    graph.set_ylabel(r'Zenith [\si{\degree}]')
     graph.set_title('Number of simulations per energy and zenith angle')
-    if particle_id is None:
-        graph.save_as_pdf('energy_zenith')
+    if particle is not None:
+        graph.save_as_pdf('energy_zenith_%s' % particle)
     else:
-        graph.save_as_pdf('energy_zenith_%s' % name(particle_id))
+        graph.save_as_pdf('energy_zenith')
 
 
-def plot_energy_zenith_per_particle(energy, zenith, particle):
-    # Angles vs Energies per Particle
-    unique_particles = set(particle)
+def plot_energy_zenith_per_particle(cq):
+    """Energy v zenith per primary particle"""
 
-    for unique_particle in unique_particles:
-        s_energy = energy.compress(particle == unique_particle)
-        s_zenith = zenith.compress(particle == unique_particle)
-        plot_energy_zenith(s_energy, s_zenith, particle_id=unique_particle)
+    for particle in cq.all_particles:
+        plot_energy_zenith(cq, particle=particle)
 
 
-def plot_n_leptons(energy, zenith, particle, n_leptons):
+def plot_n_leptons(cq):
     """Energy, Zenith"""
-    color_styles = ('black,', 'red,', 'blue,', 'orange,', 'gray,', 'green,', 'purple,')
-    line_styles = ('solid', 'dashed', 'dotted')
-    unique_energy = get_unique(energy)
-    unique_zenith = get_unique(zenith)
+
+    color_styles = cycle(('black,', 'red,', 'blue,', 'orange,', 'gray,',
+                          'green,',))
+
     n_bins = numpy.linspace(0, 8, 300)
     graph = artist.Plot()
-    for j, e in enumerate(unique_energy):
-        for i, z in enumerate(unique_zenith[::3]):
-            n_l = n_leptons.compress((energy == e) & (zenith == z) &
-                                     (particle == 14))
-            if len(n_l) < 200:
+    energies = sorted(cq.available_parameters('energy'))
+    for e, c in zip(energies, color_styles):
+        zeniths = sorted(cq.available_parameters('zenith', energy=e))
+        line_styles = cycle(('solid', 'dashed', 'dotted', 'densely dashed'))
+        for z, l in zip(zeniths[::3], line_styles):
+            sims = cq.simulations(energy=e, zenith=z)
+            n_leptons = sims['n_muon'] + sims['n_electron']
+            if len(n_leptons) < 200:
                 continue
-            counts, n_bins = numpy.histogram(numpy.log10(n_l), bins=n_bins, density=True)
-            graph.histogram(counts, n_bins, linestyle=color_styles[j % 5] + line_styles[i % 3])
-    graph.set_xlimits(min=0, max=8)
+            counts, n_bins = numpy.histogram(numpy.log10(n_leptons),
+                                             bins=n_bins, density=True)
+            graph.histogram(counts, n_bins,
+                            linestyle=c + l)
+    graph.set_xlimits(min=0, max=9)
     graph.set_ylimits(min=0)
     graph.set_xlabel('Number of leptons [N]')
     graph.set_ylabel('Count')
-    graph.set_title('Distribution of number of leptons per energy and zenith angle')
+#     graph.set_title('Distribution of number of leptons per energy and '
+#                     'zenith angle')
     graph.save_as_pdf('n_leptons')
-
-
-def get_data(overview):
-    seed1 = overview.root.simulations.col('seed1')
-    seed2 = overview.root.simulations.col('seed2')
-    seeds = numpy.array(['%d_%d' % (s1, s2) for s1, s2  in zip(seed1, seed2)])
-    particle = overview.root.simulations.col('particle_id')
-    energy = numpy.log10(overview.root.simulations.col('energy'))
-    zenith = overview.root.simulations.col('zenith')
-    azimuth = overview.root.simulations.col('azimuth')
-    n_leptons = (overview.root.simulations.col('n_electron') +
-                 overview.root.simulations.col('n_muon'))
-    return seeds, particle, energy, zenith, azimuth, n_leptons
-
-
-def get_unique(parameter):
-    return sorted(list(set(parameter)))
 
 
 def get_random_seed(seeds, particle, energy, zenith, e, z):
@@ -178,11 +175,10 @@ def get_seed_matrix(seeds, particle, energy, zenith):
 
 if __name__ == '__main__':
     with tables.open_file(OVERVIEW, 'r') as overview:
-        seeds, particle, energy, zenith, azimuth, n_leptons = get_data(overview)
-    plot_n_leptons(energy, zenith, particle, n_leptons)
-    plot_energy_zenith(energy, zenith, particle_id=14)
-#     plot_energy(energy)
-#     plot_zenith(zenith)
+        cq = CorsikaQuery(overview)
+        plot_n_leptons(cq)
+        plot_energy_zenith(cq)
+        plot_energy_zenith_per_particle(cq)
+
 #     plot_azimuth(azimuth)
-#     plot_energy_zenith_per_particle(energy, zenith, particle)
 #     get_seed_matrix(seeds, particle, energy, zenith)
