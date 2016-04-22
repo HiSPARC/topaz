855de5f83487f3e0d292d2a8ff25dcf922921bde
Arne de Laat <adelaat@nikhef.nl>
2016-04-22 22:08:58 +0200

    Clarify calculation of changed GPS for same layout

--- 150714_moving_detectors/compute_layout.py
+++ 150714_moving_detectors/compute_layout.py
@@ -1,3 +1,10 @@
+"""Calculate station layout if detectors did not move but GPS antenna has
+
+If station detectors remain in place but a different GPS antenna is used,
+or the antenna is moved, this can be used to easily calculate the new
+relative detector positions.
+
+"""
 from sapphire.clusters import HiSPARCStations
 from sapphire.transformations.axes import cartesian_to_compass
 from sapphire.transformations.clock import gps_to_datetime
@@ -21,13 +28,16 @@ if __name__ == "__main__":
     cluster.set_timestamp(OTHER_GPS)
     x1, y1 = station.get_xy_coordinates()
 
+    print 'Station layout was good on', gps_to_datetime(GOOD_GPS)
+    print 'Station layout to be calculated for', gps_to_datetime(OTHER_GPS)
+
     dx = x1 - x0
     dy = y1 - y0
-    print dx, dy
+    print 'GPS position moved by: %.3f m, %.3f m.' % (dx, dy)
 
+    print 'New relative detector coordinates'
+    cluster.set_timestamp(OTHER_GPS)
     for d in station.detectors:
-        cluster.set_timestamp(OTHER_GPS)
         d.x[d.index] -= dx
         d.y[d.index] -= dy
-        print gps_to_datetime(OTHER_GPS)
         print cartesian_to_compass(d.x[d.index], d.y[d.index], d.z[d.index])
