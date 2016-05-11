0d746c90871fbacb77e9b0a2fc10816e2173d61d
Arne de Laat <adelaat@nikhef.nl>
2016-05-11 14:17:07 +0200

    Use sapphire qsub module

--- 150624_energy_simulations/qsub.py
+++ 150624_energy_simulations/qsub.py
@@ -2,6 +2,8 @@ import os
 import textwrap
 import subprocess
 
+from sapphire import qsub
+
 
 SCRIPT_TEMPLATE = textwrap.dedent("""\
     #!/usr/bin/env bash
@@ -16,46 +18,13 @@ SCRIPT_TEMPLATE = textwrap.dedent("""\
     sleep $[ ( $RANDOM % 60 ) + 60 ]""")
 
 
-
-def get_script_path(seed):
-    script_name = 'spa_sim_{seed}.sh'.format(seed=seed)
-    script_path = os.path.join('/tmp', script_name)
-    return script_path
-
-
-def delete_script(seed):
-    """Delete script after creating job on Stoomboot"""
-    script_path = get_script_path(seed)
-    os.remove(script_path)
-
-
-def create_script(seed):
-    """Create script as temp file to run on Stoomboot"""
-
-    script_path = get_script_path(seed)
-
-    with open(script_path, 'w') as script:
-        script.write(SCRIPT_TEMPLATE.format(seed=seed))
-    os.chmod(script_path, 0774)
-
-    return script_path
-
-
 def submit_job(seed):
     """Submit job to Stoomboot"""
 
-    script_path = create_script(seed)
+    script = SCRIPT_TEMPLATE.format(seed=seed)
+    name = 'spa_sim_{name}'.format(name=seed)
 
-    qsub = ('qsub -q long -V -z -j oe -N spa_sim_{name} {script}'
-            .format(name=seed, script=script_path))
-
-    result = subprocess.check_output(qsub, stderr=subprocess.STDOUT,
-                                     shell=True)
-    if not result == '':
-        logger.error('%s - Error occured: %s' % (seed, result))
-        raise Exception
-
-    delete_script(seed)
+    qsub.submit_job(script, name, queue='long')
 
 
 if __name__ == "__main__":
