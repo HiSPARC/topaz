8efd06be206e2fefbfb98dc6b4f90d3bc04cbc0e
Arne de Laat <arne@delaat.net>
2016-07-21 15:11:07 +0200

    Add finding combinations of three stations close together

--- 150930_coincidences_distance/station_distances.py
+++ 150930_coincidences_distance/station_distances.py
@@ -12,7 +12,7 @@ from numpy import array, sqrt, testing
 
 from sapphire import HiSPARCNetwork, HiSPARCStations
 
-MIN_DISTANCE = 30
+MIN_DISTANCE = 0
 MAX_DISTANCE = 2e3
 
 
@@ -43,7 +43,7 @@ def close_pairs_in_cluster(cluster, min=MIN_DISTANCE, max=MAX_DISTANCE):
             testing.assert_allclose(station.get_lla_coordinates(),
                                     (0., 0., 0.), atol=1e-7)
         except AssertionError:
-            # Not invalid GPS
+            # Valid GPS
             station_numbers.append(station.number)
             coordinates.append(array(station.calc_center_of_mass_coordinates()))
     return get_close_pairs(zip(station_numbers, coordinates), min, max)
@@ -53,6 +53,39 @@ def get_close_pairs(coordinates, min=MIN_DISTANCE, max=MAX_DISTANCE):
     pairs = [(sc1[0], sc2[0]) for sc1, sc2 in combinations(coordinates, 2)
              if min < distance(sc1[1], sc2[1]) < max]
     return pairs
+
+
+def close_triples_in_network(min=MIN_DISTANCE, max=MAX_DISTANCE):
+    """Find triples of stations
+
+    The distances between each of the station pairs in the set must be within
+    the min and max value.
+
+    """
+    cluster = HiSPARCNetwork(force_stale=True)
+    return close_triples_in_cluster(cluster, min, max)
+
+
+def close_triples_in_cluster(cluster, min=MIN_DISTANCE, max=MAX_DISTANCE):
+    station_numbers = []
+    coordinates = []
+    for station in cluster.stations:
+        try:
+            testing.assert_allclose(station.get_lla_coordinates(),
+                                    (0., 0., 0.), atol=1e-7)
+        except AssertionError:
+            # Not invalid GPS
+            station_numbers.append(station.number)
+            coordinates.append(array(station.calc_center_of_mass_coordinates()))
+    return get_close_triples(zip(station_numbers, coordinates), min, max)
+
+
+def get_close_triples(coordinates, min=MIN_DISTANCE, max=MAX_DISTANCE):
+    triples = [(sc1[0], sc2[0], sc3[0]) for sc1, sc2, sc3 in combinations(coordinates, 3)
+               if all(min < distance(sp1, sp2) < max
+                      for sp1, sp2 in combinations((sc1[1], sc2[1], sc3[1]), 2))]
+    return triples
+
 
 
 def distance(c1, c2):
---
+++ 150930_coincidences_distance/triples.md
@@ -0,0 +1,21 @@
+# List of triples with maximum station distances of 1 km
+
+Removed stations 521 and 522 (equal to 501/510), and 1102 (bad GPS location).
+
+Single triples
+ (102, 104, 105) Zaanlands
+ (1002, 1003, 1008) Utrecht
+ (2101, 2102, 2103) Venray
+ (3201, 3202, 3203) Middelharnis
+ (7001, 7002, 7003) Twente
+ (13001, 13003, 13008) UK
+ (20001, 20002, 20003) Aarhus
+ 
+Amsterdam
+ (3, 7, 22, 23)
+
+Eindhoven
+ (8001, 8004, 8008,8009)
+
+Science Park
+ (501, 502, 503, 504, 505, 506, 507, 508, 509, 510, 511)
